5f84c1c6b2935dc1810d3f313c62143312834f22
Moxsahil <sahilmk01@gmail.com>
2024-11-12 00:04:07 +0530

    small changes

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -51,7 +51,7 @@ const Contact = () => {
           {/* form */}
           <div className="xl:w-[54%] order-2 xl:order-none">
             <form className="flex flex-col gap-6 p-10 bg-[#27272c] rounded-xl">
-              <h3 className="text-4xl text-accent">Let's work together</h3>
+              <h3 className="text-4xl text-accent">Lets work together</h3>
               <p className="text-white/60">Hire me for your next project.Fill the form below and I will get back to you as soon as possible.
               </p>
               {/* Input */}
